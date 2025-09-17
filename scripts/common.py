ea6023a55d81c0ab2def6576c1d3fb3d2682bb75
Павел Ральников <112890673+PaulRalnikov@users.noreply.github.com>
2025-09-17 17:29:34 +0300

    FEAT: all-to-all simulation config generator (#449)
    closes #448

---
+++ scripts/common.py
@@ -0,0 +1,20 @@
+import yaml
+import sys
+
+# Reads yaml from file by given path
+def load_config(path : str) -> dict:
+    """Loads yaml conig from given file"""
+    try:
+        with open(path, 'r') as f:
+            return yaml.safe_load(f)
+    except FileNotFoundError:
+        print(f"Error: Configuration file '{path}' not found.")
+        sys.exit(1)
+    except yaml.YAMLError as e:
+        print(f"Error: Invalid YAML in configuration file: {e}")
+        sys.exit(1)
+
+def save_yaml(data, filename):
+    """Save data as YAML to a file"""
+    with open(filename, "w") as f:
+        yaml.dump(data, f, sort_keys=False, default_flow_style=False)
