41de9fa045b200214713bd6441866b05076d3ea8
Pavel Ralnikov <ralnikovp@gmail.com>
2025-10-12 13:02:01 +0300

    Fix pipeline

---
+++ scripts/install_deps/plots.sh
@@ -0,0 +1,3 @@
+sudo apt install --fix-missing gnuplot
+sudo apt install --fix-missing graphviz
+pip install graphviz
---
+++ scripts/install_deps/userver.sh
@@ -0,0 +1,1 @@
+sudo apt install --allow-downgrades -y $(cat external/userver/scripts/docs/en/deps/ubuntu-24.04.md | tr '\n' ' ')
